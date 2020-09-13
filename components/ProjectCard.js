587ef70a6356675b1c2b46069686d6b973750dc3
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2020-09-13 15:01:14 -0500

    projectCard component completed, portfolio page completed

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -5,7 +5,13 @@ export default function ProjectCard(props) {
   return (
     <div className={styles.card}>
       <Link href={props.href}>
-        <h2>{props.title}</h2>
+        <a>
+          <h2>{props.title}</h2>
+        </a>
+      </Link>
+      <p>{props.desc}</p>
+      <Link href={props.github}>
+        <a>Check out the code on Github</a>
       </Link>
     </div>
   );
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -5,14 +5,14 @@ import styles from '../styles/portfolio.module.css';
 export default function Portfolio() {
   return (
     <Layout>
-      <h1 className="h1">Projects</h1>
+      <h1>Projects</h1>
       <div className={styles.projects}>
-        <ProjectCard title="Gobble" href="http://gobble.vercel.app">
-          <p>
-            A side project created for users to choose meals for a fictional
-            weekly meal kit service.
-          </p>
-        </ProjectCard>
+        <ProjectCard
+          desc="A side project created for users to choose meals for a fictional weekly meal kit service."
+          github="https://github.com/Taouen/gobble"
+          href="http://gobble.vercel.app"
+          title="Gobble"
+        />
       </div>
     </Layout>
   );
