af12e4dd53fda1abdebbf582c3d89a95ff18d0b4
Tanner Wiltshire <tanner.wiltshire@gmail.com>
2021-05-30 13:05:34 -0500

    Added project card for MTGA Sniffer

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -13,6 +13,12 @@ export default function Portfolio() {
         <h1 className={styles.heading}>Projects</h1>
         <div className={styles.projects}>
           <ProjectCard
+            desc="Ongoing hobby project. A tool to aid Magic: The Gathering players determine what cards may be in their opponent's hand"
+            url="http://mtgasniffer.tannerwiltshire.com"
+            github="https://github.com/Taouen/snifferMtga"
+            title="Magic: The Gathering Hand Sniffer"
+          />
+          <ProjectCard
             desc="Website for a small business run by an esthetician. Built with Next.js and TailwindCSS, using content from Contentful CMS."
             url="http://www.brettondoesnails.com"
             title="brettondoesnails.com"
